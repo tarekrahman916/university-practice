e274609859bce0f9ab7410e2f37e64605f91ade1
Md. Tarek Rahman <tarekrahman237@gmail.com>
2023-06-02 20:24:28 +0600

    [Done] setup project

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+import cors from "cors";
+
+app.use(cors());
+
+// Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function dbConnect() {
+  try {
+    await mongoose.connect(config.databaseUrl as string);
+    console.log("Database connected");
+    app.listen(config.port, () => {
+      console.log(`Application is listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log("Failed to connect database", error);
+  }
+}
+
+dbConnect();
